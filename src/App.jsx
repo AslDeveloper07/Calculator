ba0ae4ded88e25c0cb83ec5abd03e9ff5771acec
Asilbek Suvonov <aslwexking777@gmail.com>
2025-07-26 19:35:14 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Divider, Steps } from 'antd';
+
+const App= React.FC = () => (
+  <>
+
+    <Divider />
+    <Steps 
+      progressDot
+      current={1}
+      direction="vertical"
+      items={[
+        {
+          title: 'Finished',
+          description: 'This is a description. This is a description.',
+        },
+        {
+          title: 'Finished',
+          description: 'This is a description. This is a description.',
+        },
+        {
+          title: 'In Progress',
+          description: 'This is a description. This is a description.',
+        },
+        {
+          title: 'Waiting',
+          description: 'This is a description.',
+        },
+        {
+          title: 'Waiting',
+          description: 'This is a description.',
+        },
+      ]}
+    />
+  </>
+);
+
+export default App;
